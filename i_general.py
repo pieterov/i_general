38f083c961e9683e6c3922f436b9dc68b4e2059c
pieterov <pieter.overdevest@mac.com>
2024-01-21 20:56:55 +0100

    added f_var_name

--- i_general.py
+++ i_general.py
@@ -624,6 +624,33 @@ def f_check_nonnumeric_in_df(
 
 #######################################################################################################################
 
+def f_var_name(var):
+
+    """
+    Get argument name of variable assigned to function parameter. It is used in 'f_describe()' for example.
+
+    Parameters
+    ----------
+    var : any
+        Parameter name to which an object is assigned to, and of which we want the name.
+
+    Returns
+    -------
+    str
+        Name of object assigned to parameter, i.e., the argument name.
+    """
+
+    lcls = inspect.stack()[2][0].f_locals
+
+    for name in lcls:        
+        if id(var) == id(lcls[name]):
+            return name
+
+    return None
+
+#######################################################################################################################
+
+
 def f_check_na_in_df(
     
     df_input,
